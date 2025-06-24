169ca6ecf4bdb32c93814dcd27eba6befc3076c0
nurauliawati <nuraulya607@gmail.com>
2025-06-24 15:17:14 +0800

    perbaikan serta penyempurnaan dibagian tambah, hapus dan total

--- project.cpp
+++ project.cpp
@@ -12,19 +12,19 @@ struct Pelanggan {
 
 struct Node {
     Pelanggan data;
-    Node * next;
+    Node* next;
 };
 
-Node * head = nullptr;
+Node* head = nullptr;
 
 void InisialisasiData() {
-    Node * n1 = new Node {{"Aulia", "Diamond FF", 15500}, nullptr};
-    Node * n2 = new Node {{"Adel", "UC PUBG", 14000}, nullptr};
-    Node * n3 = new Node {{"Arin", "Diamond ML", 9000}, nullptr};
+    Node* n1 = new Node{{"Aulia", "Diamond FF", 15500}, nullptr};
+    Node* n2 = new Node{{"Adel", "UC PUBG", 14000}, nullptr};
+    Node* n3 = new Node{{"Arin", "Diamond ML", 9000}, nullptr};
 
     head = n1;
-    n1 -> next = n2;
-    n2 -> next = n3;
+    n1->next = n2;
+    n2->next = n3;
 
     cout << "Data pelanggan berhasil dimasukkan.\n";
     getch();
@@ -41,76 +41,135 @@ void TampilkanPelanggan() {
              << setw(15) << "Harga Item" << endl;
 
         int no = 1;
-        for (Node * p = head; p != nullptr; p = p -> next) {
+        for (Node* p = head; p != nullptr; p = p->next) {
             cout << setw(5) << no++
-                 << setw(20) << p -> data.nama
-                 << setw(20) << p -> data.namaItem
-                 << " Rp " << p -> data.hargaItem << endl;
+                 << setw(20) << p->data.nama
+                 << setw(20) << p->data.namaItem
+                 << " Rp " << p->data.hargaItem << endl;
         }
     }
 
     getch();
 }
 
-void TambahPelanggan(string nama, string item, int harga) {
-    Node * baru = new Node {{nama, item, harga}, nullptr};
+void TambahPelanggan() {
+    system("cls");
+    Node* baru = new Node;
+    cout << "Nama Pelanggan : "; cin >> ws; getline(cin, baru->data.nama);
+    cout << "Item yang dipilih : "; getline(cin, baru->data.namaItem);
+    cout << "Harga Top Up : Rp "; cin >> baru->data.hargaItem;
+    baru->next = nullptr;
+
     if (head == nullptr) {
         head = baru;
     } else {
         Node* p = head;
-        while (p -> next != nullptr)
-            p = p -> next;
+        while (p->next != nullptr)
+            p = p->next;
         p->next = baru;
     }
-    cout << "Data pelanggan baru berhasil ditambahkan.\n";
+
+    cout << "Data berhasil ditambahkan!\n";
+    getch();
+
+    TampilkanPelanggan();
 }
 
-void HapusPelanggan(int no) {
+void HapusPelanggan() {
+    system("cls");
     if (head == nullptr) {
         cout << "Data kosong!\n";
+        getch();
+        return;
+    }
+    int no;
+    cout << "\nMasukkan nomor pelanggan yang ingin dihapus : ";
+    cin >> no;
+
+    if (no < 1) {
+        cout << "Nomor tidak valid!\n";
+        getch();
         return;
     }
 
-    Node * hapus;
+    Node* hapus;
     if (no == 1) {
         hapus = head;
         head = head->next;
     } else {
-        Node * p = head;
+        Node* p = head;
         for (int i = 1; i < no - 1 && p != nullptr; i++)
             p = p->next;
+
         if (p == nullptr || p->next == nullptr) {
             cout << "Nomor tidak valid!\n";
+            getch();
             return;
         }
+
         hapus = p->next;
         p->next = hapus->next;
     }
     cout << "Data \"" << hapus->data.nama << "\" berhasil dihapus.\n";
     delete hapus;
+    getch();
+
+    TampilkanPelanggan();
 }
 
-void totalHarga() {
+void TotalHarga() {
+    system("cls");
     int total = 0;
-    for (Node * p = head; p != nullptr; p = p->next)
+    for (Node* p = head; p != nullptr; p = p->next)
         total += p->data.hargaItem;
-    cout << "Total seluruh harga top up: Rp " << total << endl;
+
+    cout << "Total seluruh harga top up : Rp " << total << endl;
+    getch();
+}
+
+void menu() {
+    system("cls");
+    cout << "Menu:\n";
+    cout << "1. Inisialisasi Data\n";
+    cout << "2. Tampilkan Pelanggan\n";
+    cout << "3. Tambah Pelanggan\n";
+    cout << "4. Hapus Pelanggan\n";
+    cout << "5. Total Harga\n";
+    cout << "6. Keluar\n";
+    cout << "Pilih: ";
 }
 
 int main() {
-    InisialisasiData();
-    TampilkanPelanggan();
+    char pilih;
+    do {
+        menu();
+        pilih = getch();
+        switch (pilih) {
+            case '1': InisialisasiData(); 
+            break;
+ 
+            case '2': TampilkanPelanggan(); 
+            break;
 
-    cout << "\nMenambah pelanggan baru...\n";
-    TambahPelanggan("Budi", "Diamond FF", 20000);
-    TampilkanPelanggan();
+            case '3': TambahPelanggan();
+            break;
 
-    cout << "\nMenghapus pelanggan nomor 2...\n";
-    HapusPelanggan(2);
-    TampilkanPelanggan();
+            case '4': HapusPelanggan(); 
+            break;
 
-        cout << "\nMenghitung total harga...\n";
-    totalHarga();
+            case '5': TotalHarga(); 
+            break;
+
+            case '6': 
+            break;
+
+            default:
+                system("cls");
+                cout << "Pilihan tidak tersedia!\n";
+                getch();
+                break;
+        }
+    } while (pilih != '6');
 
     return 0;
 }
