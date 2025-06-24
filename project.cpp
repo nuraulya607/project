f143843b284c7263462a2813023332d027241764
nurauliawati <nuraulya607@gmail.com>
2025-06-24 14:07:01 +0800

    final project

--- project.cpp
+++ project.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <conio.h>
+#include <string>
 using namespace std;
 
 struct Pelanggan {
@@ -11,25 +12,25 @@ struct Pelanggan {
 
 struct Node {
     Pelanggan data;
-    Node* next;
+    Node * next;
 };
 
-Node* head = nullptr;
+Node * head = nullptr;
 
-void inisialisasiData() {
-    Node* n1 = new Node{{"Aulia", "Diamond FF", 15500}, nullptr};
-    Node* n2 = new Node{{"Adel", "UC PUBG", 14000}, nullptr};
-    Node* n3 = new Node{{"Arin", "Diamond ML", 9000}, nullptr};
+void InisialisasiData() {
+    Node * n1 = new Node {{"Aulia", "Diamond FF", 15500}, nullptr};
+    Node * n2 = new Node {{"Adel", "UC PUBG", 14000}, nullptr};
+    Node * n3 = new Node {{"Arin", "Diamond ML", 9000}, nullptr};
 
     head = n1;
-    n1->next = n2;
-    n2->next = n3;
+    n1 -> next = n2;
+    n2 -> next = n3;
 
     cout << "Data pelanggan berhasil dimasukkan.\n";
     getch();
 }
 
-void tampilkanPelanggan() {
+void TampilkanPelanggan() {
     system("cls");
     if (head == nullptr) {
         cout << "Belum ada data pelanggan.\n";
@@ -40,42 +41,42 @@ void tampilkanPelanggan() {
              << setw(15) << "Harga Item" << endl;
 
         int no = 1;
-        for (Node* p = head; p != nullptr; p = p->next) {
+        for (Node * p = head; p != nullptr; p = p -> next) {
             cout << setw(5) << no++
-                 << setw(20) << p->data.nama
-                 << setw(20) << p->data.namaItem
-                 << "Rp " << p->data.hargaItem << endl;
+                 << setw(20) << p -> data.nama
+                 << setw(20) << p -> data.namaItem
+                 << " Rp " << p -> data.hargaItem << endl;
         }
     }
 
     getch();
 }
 
-void tambahPelanggan(string nama, string item, int harga) {
-    Node* baru = new Node{{nama, item, harga}, nullptr};
+void TambahPelanggan(string nama, string item, int harga) {
+    Node * baru = new Node {{nama, item, harga}, nullptr};
     if (head == nullptr) {
         head = baru;
     } else {
         Node* p = head;
-        while (p->next != nullptr)
-            p = p->next;
+        while (p -> next != nullptr)
+            p = p -> next;
         p->next = baru;
     }
     cout << "Data pelanggan baru berhasil ditambahkan.\n";
 }
 
-void hapusPelanggan(int no) {
+void HapusPelanggan(int no) {
     if (head == nullptr) {
         cout << "Data kosong!\n";
         return;
     }
 
-    Node* hapus;
+    Node * hapus;
     if (no == 1) {
         hapus = head;
         head = head->next;
     } else {
-        Node* p = head;
+        Node * p = head;
         for (int i = 1; i < no - 1 && p != nullptr; i++)
             p = p->next;
         if (p == nullptr || p->next == nullptr) {
@@ -91,22 +92,22 @@ void hapusPelanggan(int no) {
 
 void totalHarga() {
     int total = 0;
-    for (Node* p = head; p != nullptr; p = p->next)
+    for (Node * p = head; p != nullptr; p = p->next)
         total += p->data.hargaItem;
     cout << "Total seluruh harga top up: Rp " << total << endl;
 }
 
 int main() {
-    inisialisasiData();
-    tampilkanPelanggan();
+    InisialisasiData();
+    TampilkanPelanggan();
 
     cout << "\nMenambah pelanggan baru...\n";
-    tambahPelanggan("Budi", "Diamond FF", 20000);
-    tampilkanPelanggan();
+    TambahPelanggan("Budi", "Diamond FF", 20000);
+    TampilkanPelanggan();
 
     cout << "\nMenghapus pelanggan nomor 2...\n";
-    hapusPelanggan(2);
-    tampilkanPelanggan();
+    HapusPelanggan(2);
+    TampilkanPelanggan();
 
         cout << "\nMenghitung total harga...\n";
     totalHarga();
